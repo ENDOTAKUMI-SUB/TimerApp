fef001bea9eab1ba6da309a409a8cfe9cde866d4
遠藤拓巳 <en0605taku@gmail.com>
2019-02-10 12:25:54 +0900

    Add comment out

--- app/src/main/java/com/endo_takumi/kotlin_001/MainActivity.kt
+++ app/src/main/java/com/endo_takumi/kotlin_001/MainActivity.kt
@@ -14,29 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Assigning View elements to variables
         val textViewTime = findViewById(R.id.textViewTime) as TextView
         val buttonStart = findViewById(R.id.buttonStart) as Button
         val buttonStop = findViewById(R.id.buttonStop) as Button
         val buttonReset = findViewById(R.id.buttonReset) as Button
 
+        // Execute processing every second
         val runnable = object : Runnable {
             override fun run() {
+                // Increment one second
                 timeValue++
 
+                // Update TextView
                 timeToText(timeValue)?.let { textViewTime.text = it }
                 handler.postDelayed(this, 1000)
             }
 
         }
 
+        // Operation when START button is pushed
         buttonStart.setOnClickListener {
             handler.post(runnable)
         }
 
+        // Operation when STOP button is pushed
         buttonStop.setOnClickListener {
             handler.removeCallbacks(runnable)
         }
 
+        // Operation when RESET button is pushed
         buttonReset.setOnClickListener {
             handler.removeCallbacks(runnable)
             timeValue = 0
@@ -46,6 +53,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Function to convert numeric value to character string of 00: 00: 00 format
     private fun timeToText(time: Int = 0): String? {
         return if (time < 0) {
             null
