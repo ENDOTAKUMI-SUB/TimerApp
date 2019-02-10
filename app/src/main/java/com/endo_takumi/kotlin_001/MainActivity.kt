e3b58644ce2659a21bd32d86d8bb73fbebd2a8b0
遠藤拓巳 <en0605taku@gmail.com>
2019-02-10 15:23:21 +0900

    Added Lap function.

--- app/src/main/java/com/endo_takumi/kotlin_001/MainActivity.kt
+++ app/src/main/java/com/endo_takumi/kotlin_001/MainActivity.kt
@@ -3,12 +3,14 @@ package com.endo_takumi.kotlin_001
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val handler = Handler()
     var timeValue = 0
     var status = false
+    var timeLap = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
             if (!status) {
                 handler.post(runnable)
                 status = true
+                buttonReset.text = getString(R.string.buttonResetLap)
             }
         }
 
@@ -40,17 +43,28 @@ class MainActivity : AppCompatActivity() {
             if (status) {
                 handler.removeCallbacks(runnable)
                 status = false
+                buttonReset.text = getString(R.string.buttonReset)
             }
         }
 
         // Operation when RESET button is pushed
         buttonReset.setOnClickListener {
-            handler.removeCallbacks(runnable)
-            timeValue = 0
-            timeToText()?.let {
-                textViewTime.text = it
+            if (!status) {
+                // Reset
+                handler.removeCallbacks(runnable)
+                timeValue = 0
+                timeToText()?.let {
+                    textViewTime.text = it
+                }
+
+                timeLap = ""
+                textViewLapTime.text = timeLap
+                status = false
+            } else {
+                //Lap
+                timeLap += timeToText(timeValue) + "\n"
+                textViewLapTime.text = timeLap
             }
-            status = false
         }
     }
 
